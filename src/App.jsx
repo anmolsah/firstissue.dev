b7e4f903771bbece5ddc042a317fd358891ec726
Anmol Sah <kumarxxx242424@gmail.com>
2025-06-14 23:33:17 +0530

    feat: add Navbar component and integrate it into App feat: implement Supabase client setup with environment variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
