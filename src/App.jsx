b6a7b6f72da80946e075470b744172d3fc7fe131
Anmol Sah <kumarxxx242424@gmail.com>
2025-06-16 01:29:54 +0530

    feat: implement LandingPage component with features and stats sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/Navbar";
+import LandingPage from './pages/LandingPage';
 
 const App = () => {
   return (
+    <AuthProvider>
     <div>
       <Navbar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
+    </AuthProvider>
   );
 };
 
